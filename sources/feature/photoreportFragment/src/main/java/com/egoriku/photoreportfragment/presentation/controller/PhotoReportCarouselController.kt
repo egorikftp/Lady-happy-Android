17a990ca06aae3d04ce7876f78609422aba7d2ea
Yahor Urbanovich <egorikftp@gmail.com>
2018-10-01 22:54:14 +0300

    Remove dots view, use new page library indication

--- sources/feature/photoreportFragment/src/main/java/com/egoriku/photoreportfragment/presentation/controller/PhotoReportCarouselController.kt
+++ sources/feature/photoreportFragment/src/main/java/com/egoriku/photoreportfragment/presentation/controller/PhotoReportCarouselController.kt
@@ -2,13 +2,12 @@ package com.egoriku.photoreportfragment.presentation.controller
 
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.PagerSnapHelper
-import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.egoriku.core.model.IPhotoReportModel
 import com.egoriku.photoreportfragment.R
-import com.egoriku.photoreportfragment.presentation.view.DotsView
-import kotlinx.android.synthetic.main.adapter_item_photo_report_carousel.view.*
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.adapter_item_photo_report_carousel.*
 import ru.surfstudio.android.easyadapter.EasyAdapter
 import ru.surfstudio.android.easyadapter.ItemList
 import ru.surfstudio.android.easyadapter.controller.BindableItemController
@@ -20,46 +19,35 @@ class PhotoReportCarouselController : BindableItemController<IPhotoReportModel, 
 
     override fun getItemId(data: IPhotoReportModel) = data.hashCode().toString()
 
-    inner class Holder(parent: ViewGroup) : BindableViewHolder<IPhotoReportModel>(parent, R.layout.adapter_item_photo_report_carousel) {
+    inner class Holder(parent: ViewGroup) : BindableViewHolder<IPhotoReportModel>(parent, R.layout.adapter_item_photo_report_carousel), LayoutContainer {
 
-        private lateinit var newsModel: IPhotoReportModel
+        override val containerView: View?
+            get() = itemView
 
         private val photoReportItemController: PhotoReportItemController = PhotoReportItemController()
-
-        private val description: TextView = itemView.descriptionTextView
-        private val newsRecyclerView: RecyclerView
-        private val dots: DotsView = itemView.dotsView
-        private val newsDate: TextView = itemView.newsDate
+        private lateinit var newsModel: IPhotoReportModel
 
         init {
-            newsRecyclerView = itemView.newsRecyclerView.apply {
-                layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false).apply {
-                    initialPrefetchItemCount = 1
-                }
-
-                addOnScrollListener(object : RecyclerView.OnScrollListener() {
-                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                        super.onScrolled(recyclerView, dx, dy)
-                        // dots.updateScrollPosition(recyclerView.scrollPercentage())
-                    }
-                })
+            reportCarouselRecyclerView.apply {
+                layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             }
 
-            PagerSnapHelper().attachToRecyclerView(newsRecyclerView)
+            PagerSnapHelper().attachToRecyclerView(reportCarouselRecyclerView)
+            pagerIndicator.attachToRecyclerView(reportCarouselRecyclerView)
         }
 
         override fun bind(data: IPhotoReportModel) {
             newsModel = data
-            newsDate.text = data.date
-            description.text = data.description
-            dots.setDotCount(data.images.size)
 
-            if (newsRecyclerView.adapter == null) {
-                newsRecyclerView.adapter = EasyAdapter().apply {
+            newsDateTextView.text = data.date
+            descriptionTextView.text = data.description
+
+            if (reportCarouselRecyclerView.adapter == null) {
+                reportCarouselRecyclerView.adapter = EasyAdapter().apply {
                     setItems(ItemList.create().addAll(data.images, photoReportItemController))
                 }
             } else {
-                (newsRecyclerView.adapter as EasyAdapter)
+                (reportCarouselRecyclerView.adapter as EasyAdapter)
                         .setItems(ItemList.create().addAll(data.images, photoReportItemController))
             }
         }
