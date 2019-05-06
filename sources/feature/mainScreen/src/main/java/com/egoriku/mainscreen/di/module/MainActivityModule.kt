df4bbd30f5b84e72f4822b2f872e3815019c6eb8
Yahor Urbanovich <yahor_urbanovich@epam.com>
2019-05-06 23:39:36 +0300

    Remove commented code

--- sources/feature/mainScreen/src/main/java/com/egoriku/mainscreen/di/module/MainActivityModule.kt
+++ sources/feature/mainScreen/src/main/java/com/egoriku/mainscreen/di/module/MainActivityModule.kt
@@ -3,11 +3,4 @@ package com.egoriku.mainscreen.di.module
 import dagger.Module
 
 @Module
-class MainActivityModule {
-
-   /* @Provides
-    @IntoMap
-    @ActivityScope
-    @ViewModelKey(MainActivityViewModel::class)
-    fun provideMaindActivityViewModel(router: IRouter, analytics: IAnalytics): ViewModel = MainActivityViewModel(router, analytics)*/
-}
+class MainActivityModule
