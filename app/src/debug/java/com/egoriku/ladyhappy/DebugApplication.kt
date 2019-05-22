c717ec66a8248cdd2c1951b0d3e67d0ce5361651
Egor <egorikftp@gmail.com>
2019-05-22 22:16:21 +0300

    Task/move libraries into build src folder (#30)
    * Move libraries into buildSrc folder
    
    * update LeakCanary
    
    * Optimize StrictMode
    
    * Migrate gradle classpatch dependencies into buildSrc

--- app/src/debug/java/com/egoriku/ladyhappy/DebugApplication.kt
+++ app/src/debug/java/com/egoriku/ladyhappy/DebugApplication.kt
@@ -2,35 +2,20 @@ package com.egoriku.ladyhappy
 
 import android.annotation.SuppressLint
 import android.os.StrictMode
-import com.squareup.leakcanary.LeakCanary
 
 @SuppressLint("Registered")
 open class DebugApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        initLeakCanary()
-
-        // enableStrictMode()
-    }
-
-    private fun initLeakCanary() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return
-        }
-        LeakCanary.install(this)
+        enableStrictMode()
     }
 
     private fun enableStrictMode() {
         StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
                 .detectAll()
                 .penaltyLog()
-                .build())
-
-        StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
-                .detectAll()
-                .penaltyLog()
+                .penaltyDialog()
                 .build())
     }
 }
-
