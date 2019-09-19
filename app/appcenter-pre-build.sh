c73c74d8dfecf0bef752e69b69212a6ea780da97
Egor <egorikftp@gmail.com>
2019-09-19 21:54:51 +0300

    Update appcenter-pre-build.sh

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-if [ -e "$GOOGLE_JSON_FILE" ]
-then
-    echo "Updating Google Json"
-    echo $GOOGLE_JSON | base64 --decode > app/google-services.json
-
-    echo "File content:"
-    cat $GOOGLE_JSON_FILE
-fi
+echo "Updating Google Json"
+echo $GOOGLE_JSON | base64 --decode > app/google-services.json
