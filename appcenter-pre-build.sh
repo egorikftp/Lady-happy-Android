bd52d7401f37a3bd847f06ce4699bb57750a1dea
Egor <egorikftp@gmail.com>
2019-09-19 21:12:46 +0300

    Create appcenter-pre-build.sh

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,11 @@
+GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/app/google-services.json
+
+if [ -e "$GOOGLE_JSON_FILE" ]
+then
+    echo "Updating Google Json"
+    echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+    sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
+
+    echo "File content:"
+    cat $GOOGLE_JSON_FILE
+fi
