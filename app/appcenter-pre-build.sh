43c6eb0473d2b31e7a3d146c3e5141fd234b5535
Egor <egorikftp@gmail.com>
2019-09-19 22:13:14 +0300

    Update appcenter-pre-build.sh

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -2,3 +2,8 @@
 
 echo "Updating Google Json"
 echo $GOOGLE_JSON | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/app/google-services.json
+
+echo "Copy KeyStore"
+echo $KEY_STORE | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/keystore/lady_happy_key_store.jks
+
+echo "Files successfully copied"
