4129cf439ca9cea756b465d8eed2aaa32ceebf4d
Yahor Urbanovich <yahor_urbanovich@gmail.com>
2019-09-24 23:46:07 +0300

    Update libraries to actual version

--- buildSrc/src/main/java/ClasspathVersion.kt
+++ buildSrc/src/main/java/ClasspathVersion.kt
@@ -1,5 +1,5 @@
 object ClasspathVersion {
-    const val fabric = "1.31.0"
+    const val fabric = "1.31.1"
     const val googleServices = "4.3.2"
     const val gradleTools = "3.5.0"
     const val versionPlugin = "0.25.0"
