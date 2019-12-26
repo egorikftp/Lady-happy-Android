f9bbd4b0d74932df8ced0fc5111a5eeda25338aa
Yahor Urbanovich <egorikftp@gmail.com>
2019-12-27 00:04:00 +0300

    Call SplitCompat.install for dynamic feature

--- sources/dynamicFeature/postCreator/src/main/java/com/egoriku/ladyhappy/postcreator/PostCreatorFragment.kt
+++ sources/dynamicFeature/postCreator/src/main/java/com/egoriku/ladyhappy/postcreator/PostCreatorFragment.kt
@@ -1,7 +1,13 @@
 package com.egoriku.ladyhappy.postcreator
 
+import android.content.Context
 import androidx.fragment.app.Fragment
+import com.google.android.play.core.splitcompat.SplitCompat
 
 class PostCreatorFragment : Fragment(R.layout.fragment_post_creator) {
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        SplitCompat.install(context)
+    }
 }
