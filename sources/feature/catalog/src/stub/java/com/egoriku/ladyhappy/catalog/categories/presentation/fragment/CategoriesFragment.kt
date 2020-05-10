b614158fc8a9318e37da10ce584c7b187c6b8e61
Yahor Urbanovich <egorikftp@gmail.com>
2020-05-10 14:52:29 +0300

    Fix lint issue

--- sources/feature/catalog/src/stub/java/com/egoriku/ladyhappy/catalog/categories/presentation/fragment/CategoriesFragment.kt
+++ sources/feature/catalog/src/stub/java/com/egoriku/ladyhappy/catalog/categories/presentation/fragment/CategoriesFragment.kt
@@ -3,4 +3,4 @@ package com.egoriku.ladyhappy.catalog.categories.presentation.fragment
 import androidx.fragment.app.Fragment
 import com.egoriku.ladyhappy.catalog.R
 
-class RootCatalogFragment : Fragment(R.layout.fragment_catalog_stub)
+class CategoriesFragment : Fragment(R.layout.fragment_catalog_stub)
