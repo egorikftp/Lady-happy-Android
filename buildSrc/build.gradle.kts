117403906a63dabda5562324715293f782c8e38b
Yahor Urbanovich <egorikftp@gmail.com>
2020-08-25 10:29:02 +0300

    Update Kotlin in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,6 +22,6 @@ dependencies {
     implementation(localGroovy())
 
     implementation("com.android.tools.build:gradle:4.0.1")
-    implementation(kotlin("gradle-plugin", "1.3.72"))
+    implementation(kotlin("gradle-plugin", "1.4.0"))
     implementation(kotlin("android-extensions"))
 }
