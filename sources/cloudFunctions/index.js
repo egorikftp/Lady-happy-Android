ee967b4258002207857fc3db9d2e64c90f9079c9
Yahor Urbanovich <egorikftp@gmail.com>
2021-01-23 22:27:32 +0300

    Update firebase cloud functions according for new db scheme

--- sources/cloudFunctions/index.js
+++ sources/cloudFunctions/index.js
@@ -1,6 +1,6 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 const db = admin.firestore();
@@ -9,75 +9,79 @@ const db = admin.firestore();
  * Deploy command:
  *
  *  firebase deploy --only functions
- *  
+ *
  */
 
 /**
  * Cloud function, increment count of specific category.
  */
 exports.subcategoryOnCreate = functions.firestore
-    .document('listOfHats/{documentID}')
+    .document("listOfHats/{documentID}")
     .onCreate((change, context) => {
-        return extracted(change, OperationType.CREATE);
+      return extracted(change, OperationType.CREATE);
     });
 
 /**
  * Cloud function, decrement count of specific category.
  */
 exports.subcategoryOnDelete = functions.firestore
-    .document('listOfHats/{documentID}')
+    .document("listOfHats/{documentID}")
     .onDelete((change, context) => {
-        return extracted(change, OperationType.DELETE)
-    })
+      return extracted(change, OperationType.DELETE);
+    });
 
 const OperationType = {
-    CREATE: 1,
-    DELETE: 2
+  CREATE: 1,
+  DELETE: 2,
 };
 
+// eslint-disable-next-line require-jsdoc
 function extracted(change, operationType) {
-    const data = change.data()
+  const data = change.data();
 
-    const isHasCategoryId = data.hasOwnProperty('categoryId')
-    const isHasSubcategoryId = data.hasOwnProperty('subcategoryId')
+  // eslint-disable-next-line no-prototype-builtins
+  const isHasCategoryId = data.hasOwnProperty("categoryId");
+  // eslint-disable-next-line no-prototype-builtins
+  const isHasSubcategoryId = data.hasOwnProperty("subCategoryId");
+  if (!isHasCategoryId) {
+    console.log("egorik: ", "categoryId null");
+    return;
+  }
+  if (!isHasSubcategoryId) {
+    console.log("egorik: ", "subcategoryId null");
+    return;
+  }
 
-    console.log("egorik: ", isHasCategoryId, isHasSubcategoryId)
+  const {categoryId, subCategoryId} = data;
+  console.log("egorik: ", categoryId, subCategoryId);
 
-    if (!isHasCategoryId || !isHasSubcategoryId) {
-        console.log("egorik: ", "categoryId or subcategoryId null")
-        return;
-    }
+  db.collection("subcategories")
+      .where("categoryId", "==", categoryId)
+      .where("subCategoryId", "==", subCategoryId)
+      .get()
+      .then(function(querySnapshot) {
+        const documentSnapshot = querySnapshot.docs[0];
+        // eslint-disable-next-line max-len
+        let currentCount = documentSnapshot._fieldsProto.publishedCount.integerValue;
+        console.log("egorik: currentCount=", currentCount);
 
-    const categoryId = data.categoryId
-    const subcategoryId = data.subcategoryId
-    console.log("egorik: ", categoryId, subcategoryId)
+        let newCount;
+        if (operationType === OperationType.CREATE) {
+          console.log("egorik: operation type = Create");
+          newCount = ++currentCount;
+        } else if (operationType === OperationType.DELETE) {
+          console.log("egorik: operation type = Delete");
+          newCount = --currentCount;
+        }
 
-    db.collection('subcategories')
-        .where('categoryId', '==', categoryId)
-        .where('subcategoryId', '==', subcategoryId)
-        .get()
-        .then(function (querySnapshot) {
-            const documentSnapshot = querySnapshot.docs[0];
-            let currentCount = documentSnapshot._fieldsProto.publishedCount.integerValue
-            console.log("egorik: currentCount=", currentCount)
+        console.log("egorik: newCount =", newCount);
+        console.log("egorik: id =", documentSnapshot.id);
 
-            let newCount;
-            if (operationType === OperationType.CREATE) {
-                console.log("egorik: operation type = Create")
-                newCount = ++currentCount
-            } else if (operationType === OperationType.DELETE) {
-                console.log("egorik: operation type = Delete")
-                newCount = --currentCount
-            }
-
-            console.log("egorik: newCount=", newCount)
-            console.log("egorik: id=", documentSnapshot.id)
-
-            return db.collection('subcategories')
-                .doc(documentSnapshot.id)
-                .set({publishedCount: newCount}, {merge: true})
-        })
-        .catch(function (error) {
-            console.log("egorik: Error getting documents ", error);
-        });
+        return db.collection("subcategories")
+            .doc(documentSnapshot.id)
+            .set({publishedCount: newCount}, {merge: true});
+      })
+      .catch(function(error) {
+        console.log("egorik: Error getting documents ", error);
+      });
 }
