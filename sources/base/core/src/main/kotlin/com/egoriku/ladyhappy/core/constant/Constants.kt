21f180fcfe8b2dcb25ae6cbfa2f847a4f817c33d
Yahor Urbanovich <egorikftp@gmail.com>
2021-02-08 12:11:38 +0300

    extract CollectionPath constants

--- sources/base/core/src/main/kotlin/com/egoriku/ladyhappy/core/constant/Constants.kt
+++ sources/base/core/src/main/kotlin/com/egoriku/ladyhappy/core/constant/Constants.kt
@@ -4,3 +4,7 @@ const val DYNAMIC_FEATURE_POST_CREATOR = "postCreator"
 
 const val REQUEST_KEY_DYNAMIC_FEATURE = "REQUEST_KEY_DYNAMIC_FEATURE"
 const val RESULT_KEY_DYNAMIC_FEATURE = "RESULT_KEY_DYNAMIC_FEATURE"
+
+object CollectionPath {
+    const val ALL_HATS = "listOfHats"
+}
--- sources/dynamicFeature/postCreator/src/main/kotlin/com/egoriku/ladyhappy/postcreator/data/remote/PublishPostRepository.kt
+++ sources/dynamicFeature/postCreator/src/main/kotlin/com/egoriku/ladyhappy/postcreator/data/remote/PublishPostRepository.kt
@@ -1,6 +1,7 @@
 package com.egoriku.ladyhappy.postcreator.data.remote
 
 import com.egoriku.ladyhappy.core.IFirebase
+import com.egoriku.ladyhappy.core.constant.CollectionPath.ALL_HATS
 import com.egoriku.ladyhappy.extensions.logDm
 import com.egoriku.ladyhappy.network.ResultOf
 import com.egoriku.ladyhappy.postcreator.data.entity.UploadEntity
@@ -8,15 +9,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
-private const val COLLECTION_ALL_HATS = "listOfHats"
-
 class PublishPostRepository(firebase: IFirebase) {
 
     private val reference = firebase.firebaseFirestore
 
     suspend fun publish(uploadEntity: UploadEntity): ResultOf<Boolean> = withContext(Dispatchers.IO) {
         runCatching {
-            reference.collection(COLLECTION_ALL_HATS)
+            reference.collection(ALL_HATS)
                     .document()
                     .set(uploadEntity)
                     .addOnFailureListener { e -> logDm("Error adding document $e") }
