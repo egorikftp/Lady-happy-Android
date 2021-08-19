f949407a1104b9a66fd3b0221c9f65427e15d93c
Yahor Urbanovich <egorikftp@gmail.com>
2021-08-19 11:18:17 +0300

    Rollback update GradlePlugins

--- buildSrc/src/main/java/GradlePlugins.kt
+++ buildSrc/src/main/java/GradlePlugins.kt
@@ -3,11 +3,11 @@ import com.egoriku.versions.LibrariesVersion
 object GradlePluginsVersion {
     const val firebaseCrashlytics = "2.6.1"
     const val firebasePerformance = "1.4.0"
-    const val detekt = "1.17.1"
+    const val detekt = "1.17.0"
     const val googleServices = "4.3.8"
     const val gradleTools = "4.2.1"
     const val ossLicenses = "0.10.4"
-    const val versionPlugin = "0.39.0"
+    const val versionPlugin = "0.38.0"
 }
 
 object GradlePlugins {
